81b22be17c86f74532968dd0a9e9d3a809ce7e7f
Jannick <jannick.apitz@gmail.com>
2016-10-24 10:33:35 +0200

    Pfade korrigiert

--- functions.php
+++ functions.php
@@ -2,29 +2,29 @@
 function getSite($site){
 	switch ($site){
 		case home:
-			echo "include 'home.php'"; //TODO: home.php
+			echo "include 'Gruppen_de/home.php'"; //TODO: home.php
 			break;
 		case sport:
-			echo "include 'Elemente_mit_Wetten.php'";
+			echo "include 'Gruppen_de/Elemente_mit_Wetten.php'";
 			break;
 		case kurse:
-			echo "include 'Elemente_mit_Kursen.php'";
+			echo "include 'Gruppen_de/Elemente_mit_Kursen.php'";
 			break;
 		case statistik:
-			echo "include 'Container_mit_Statistiken.php'";
+			echo "include 'Gruppen_de/Container_mit_Statistiken.php'";
 			break;
 		case profil:
-			echo "include 'Container_mit_Statistiken.php'";
+			echo "include 'Gruppen_de/Container_mit_Statistiken.php'";
 			break;
 		case logout:
-			echo "include 'home.php'"; //TODO: home.php
+			echo "include 'Gruppen_de/home.php'"; //TODO: home.php
 			logout(); //TODO: Funktion logout() schreiben
 			break;
 		case login:
-			echo "include 'Anmeldungen-Registrieren-Einstellungen.php'";
+			echo "include 'Gruppen_de/Anmeldungen-Registrieren-Einstellungen.php'";
 			break;
 		case rechtliches:
-			echo "include 'Rechtliches.php'";
+			echo "include 'Gruppen_de/Rechtliches.php'";
 			getContentRechtliches($_GET['topic']); //TODO getContentRechtliches() schreiben
 			break;
 	}
